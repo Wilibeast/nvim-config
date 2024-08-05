4b656d075e8c12bd237690a273607405f86db176
William Bence <wbence1@gmail.com>
2024-08-06 00:18:33 +0200

    updated mapping descriptions

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,12 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 --- Mapping for Ctrl + Enter to trigger code action
-vim.api.nvim_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true , desc = "Code action"})
+vim.api.nvim_set_keymap('n', '<leader>rf', '<cmd>Telescope lsp_references<CR>', { noremap = true, silent = true , desc = "References"})
 
-vim.api.nvim_set_keymap('n', '<leader>dt', "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>dc', "<cmd>lua require'dap'.continue()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>do', "<cmd>lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>di', "<cmd>lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dt', "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true , desc = "Toggle breakpoint"})
+vim.api.nvim_set_keymap('n', '<leader>dc', "<cmd>lua require'dap'.continue()<CR>", { noremap = true, silent = true , desc = "Continue"})
+vim.api.nvim_set_keymap('n', '<leader>do', "<cmd>lua require'dap'.step_over()<CR>", { noremap = true, silent = true , desc = "Step over"})
+vim.api.nvim_set_keymap('n', '<leader>di', "<cmd>lua require'dap'.step_into()<CR>", { noremap = true, silent = true , desc = "Step into"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
